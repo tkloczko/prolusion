673d2fbfcd693ffeca653c760713dad0255dee2e
Julien Wintz <julien.wintz@inria.fr>
2015-11-21 03:09:34 +0100

    Updating melpa urls.

--- prolusion-core/prolusion-packages.el
+++ prolusion-core/prolusion-packages.el
@@ -18,9 +18,9 @@
 
 (setq package-user-dir prolusion-elpa-dir)
 
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (package-initialize)
 
