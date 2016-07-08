b2144730e9a89820b02285a747ba976c00781693
Julien Wintz <julien.wintz@inria.fr>
2016-07-08 17:49:52 +0200

    Using only melpa instead of melpa-stable.

--- prolusion-core/prolusion-packages.el
+++ prolusion-core/prolusion-packages.el
@@ -28,7 +28,6 @@
 
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (package-initialize)
 
